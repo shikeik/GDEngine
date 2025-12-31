6e88de005f37345a169741107edfc2d421db20e5
shikeik <2667097598@qq.com>
2025-12-31 16:01:11 +0800

    projectVersion=1.5.0-beta

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SoloFight V1.5.0-alpha1
+# SoloFight V1.5.0-beta
 
 ## 介绍:
 
--- core/src/main/java/com/goldsprite/solofight/BuildConfig.java
+++ core/src/main/java/com/goldsprite/solofight/BuildConfig.java
@@ -1,6 +1,6 @@
 package com.goldsprite.solofight;
 public class BuildConfig {
 	public static final String PROJECT_NAME = "SoloFight";
-	public static final String DEV_VERSION = "1.5.0-alpha1";
+	public static final String DEV_VERSION = "1.5.0-beta";
 	public static final String JDK_VERSION = "17";
 }
